7c280bf3c47ac5b631be3e2557e48a068795eba2
Nima_nmz <namazian.n@gmail.com>
2026-08-27 11:03:47 +0330

    add sectionCard comp

---
+++ components/ui/SectionCard.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{ title?: string }>()
+</script>
+
+<template>
+  <div class="section-card">
+    <h3 v-if="title">{{ title }}</h3>
+    <slot />
+  </div>
+</template>
