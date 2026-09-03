b092f40ff4024b5ebc30565796dc2ce0f7c434b4
Nima_nmz <namazian.n@gmail.com>
2026-09-03 10:29:29 +0330

    use shadcn Button

--- components/loan/LoanProductCard.vue
+++ components/loan/LoanProductCard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { LoanProduct } from '~/types/loan'
 import { useLoanFormatter } from '~/composables/useLoanFormatter'
+import { Button } from '@/components/ui/button'
 
 defineProps<{ product: LoanProduct; selected: boolean }>()
 defineEmits<{ select: [] }>()
@@ -9,7 +10,7 @@ const { formatAmount } = useLoanFormatter()
 </script>
 
 <template>
-  <button type="button" class="product-card" :class="{ selected }" @click="$emit('select')">
+  <Button type="button" variant="outline" class="product-card w-full justify-start"  :class="{ selected }" @click="$emit('select')">
     <div class="product-card-head">
       <span class="product-title">{{ product.title }}</span>
       <span class="product-rate">{{ product.interestRate }}٪ سود سالانه</span>
@@ -19,14 +20,14 @@ const { formatAmount } = useLoanFormatter()
       <span>{{ formatAmount(product.minAmount) }} تا {{ formatAmount(product.maxAmount) }}</span>
       <span>{{ product.minDurationMonths }} تا {{ product.maxDurationMonths }} ماه</span>
     </div>
-  </button>
+  </Button>
 </template>
 
 <style scoped>
 @reference "~/assets/css/main.css";
 
 .product-card {
-  @apply block w-full cursor-pointer rounded-xl border-2 border-gray-200 bg-white p-4 text-right font-[inherit] transition-[border-color,background-color] duration-200;
+  @apply block w-full h-28 cursor-pointer rounded-xl border-2 border-gray-200 bg-white p-4 text-right transition-[border-color,background-color] duration-200;
 }
 .product-card:hover {
   @apply border-blue-300;
