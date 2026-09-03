4ee9c2e61f480626be6e30dabdc967d5b0ae9671
Nima_nmz <namazian.n@gmail.com>
2026-09-03 10:37:46 +0330

    feat loading to Button component

--- components/ui/button/Button.vue
+++ components/ui/button/Button.vue
@@ -10,10 +10,12 @@ interface Props extends /* @vue-ignore */ PrimitiveProps {
   variant?: ButtonVariants["variant"]
   size?: ButtonVariants["size"]
   class?: HTMLAttributes["class"]
+  loading?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
   as: "button",
+  loading: false,
 })
 </script>
 
@@ -24,8 +26,23 @@ const props = withDefaults(defineProps<Props>(), {
     :data-size="size"
     :as="as"
     :as-child="asChild"
+    :disabled="loading || ($attrs as any).disabled"
     :class="cn(buttonVariants({ variant, size }), props.class)"
   >
+    <span v-if="loading" class="btn-loading-spinner" />
     <slot />
   </Primitive>
 </template>
+
+<style scoped>
+@reference "~/assets/css/main.css";
+
+.btn-loading-spinner {
+  @apply h-4 w-4 shrink-0 rounded-full border-2 border-current border-t-transparent;
+  animation: btn-spin 0.6s linear infinite;
+}
+
+@keyframes btn-spin {
+  to { transform: rotate(360deg); }
+}
+</style>
